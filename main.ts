347d36e70470488cec8f0a728cfb67e303dc2246
Shane MacPhillamy <rcoaic@gmail.com>
2022-10-09 12:00:25 +1030

    Finesse control.

--- main.ts
+++ main.ts
@@ -3,15 +3,22 @@ DFRobotMaqueenPlusV2.onIrDatagram(function (message) {
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 100)
     }
     if (message == 25) {
+        DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+        basic.pause(200)
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eBackward, 100)
     }
     if (message == 20) {
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 100)
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
+        basic.pause(200)
     }
     if (message == 22) {
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 100)
+        basic.pause(200)
+    }
+    if (message == 2) {
+        DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
     }
 })
 DFRobotMaqueenPlusV2.init()
