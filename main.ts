6412fd3c3ae4b8a3698957bdd5eefec0ca2de904
Shane MacPhillamy <rcoaic@gmail.com>
2022-10-09 11:10:08 +1030

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+DFRobotMaqueenPlusV2.onIrDatagram(function (message) {
+    if (message == 17) {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 100)
+    }
+    if (message == 25) {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eBackward, 100)
+    }
+    if (message == 20) {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 100)
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
+    }
+    if (message == 22) {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 100)
+    }
+})
+DFRobotMaqueenPlusV2.init()
+DFRobotMaqueenPlusV2.ledRainbow(1, 360)
+DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eOpen)
